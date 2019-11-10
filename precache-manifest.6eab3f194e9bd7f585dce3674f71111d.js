8b520e0b3ec7d9dd0b4442196ab68f2699152703
GroverTB <grover.jtb@gmail.com>
2019-11-10 12:39:39 -0500

    Updates

---
+++ precache-manifest.6eab3f194e9bd7f585dce3674f71111d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2a72911e719ca84e2f039c8d4080b043",
+    "url": "/tvperuplay-web/index.html"
+  },
+  {
+    "revision": "383b581e5c6c593e7cd5",
+    "url": "/tvperuplay-web/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "a89ac9934bcc9127411a",
+    "url": "/tvperuplay-web/static/js/2.73d5e448.chunk.js"
+  },
+  {
+    "revision": "383b581e5c6c593e7cd5",
+    "url": "/tvperuplay-web/static/js/main.6c082883.chunk.js"
+  },
+  {
+    "revision": "475080691ab143cb801b",
+    "url": "/tvperuplay-web/static/js/runtime-main.ecb985e5.js"
+  },
+  {
+    "revision": "2f3b3ac0394d6d9b8fa1c08e926b9a6b",
+    "url": "/tvperuplay-web/static/media/banner-bg.2f3b3ac0.png"
+  },
+  {
+    "revision": "81878ee0f594eb3fdac49f6cc9cd3fdd",
+    "url": "/tvperuplay-web/static/media/icon.81878ee0.png"
+  },
+  {
+    "revision": "b795284ae03d9bb85e7c6ac370ab5b82",
+    "url": "/tvperuplay-web/static/media/iphone_hand.b795284a.png"
+  }
+]);
